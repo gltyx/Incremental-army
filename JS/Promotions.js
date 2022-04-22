2a6fccd89bf60184ea77910f8e6d19991464f4a2
MrBacon470 <mrbacon470@outlook.com>
2022-04-21 22:00:21 -0500

    More upgrade stuff

--- JS/Promotions.js
+++ JS/Promotions.js
@@ -1,8 +1,8 @@
 const levelNameList = ["Bird Colonel","Brigadier General","Major General","Lieutenant General","General","General of the Army"]
 const levelImgSrcs = ['Imgs/colonel.svg','Imgs/brigadier_general.svg','Imgs/major_general.svg','Imgs/lieutenant_general.svg','Imgs/general.svg','Imgs/general_of_the_army.svg']
 const promotionReqs = [D(1e3),D(1e4),D(1e5),D(1e6),D(1e7)]
-const promotionDescs = ['Automated Enlistment','Propaganda only Costs 50% of Funds','Automated Fabrication','Automated Acquisition','Medal Gain 2x','Automated Hiring','','5x Base Funding','Automated Funding',
-'','','','Being Researched','Being Researched','Being Researched']
+const promotionDescs = ['Automated Enlistment','Propaganda only Costs 50% of Funds','Automated Fabrication','Automated Acquisition','Medal Gain 2x','Automated Hiring','Propaganda only costs 25% of Funds','5x Base Funding','Automated Funding',
+'Medal Gain 4x','Decrease Cost Scaling on Enlisted','','Being Researched','Being Researched','Being Researched']
 const promotionCosts = [D(100),D(500),D(1e3),D(5e3),D(5e3),D(1e4),D(1e5),D(2.5e5),D(5e5),D(1e6),D(2.5e6),D(5e6),D(1e7),D(2.5e7),D(5e7)]
 
 function updatePromotionButtons() {
